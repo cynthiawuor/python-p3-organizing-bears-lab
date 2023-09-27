a80b457a1922132ea20b99e88fe8c48f3c114197
cynthiawuor <cynthiawuor254@gmail.com>
2023-09-27 11:08:24 +0300

    failed

---
+++ lib/create.sql
@@ -0,0 +1,11 @@
+CREATE TABLE bears (
+  | column      | type    |
+| ----------- | ------- |
+| id          | integer |
+| name        | text    |
+| age         | integer |
+| sex         | text    |
+| color       | text    |
+| temperament | text    |
+| alive       | boolean |
+);
---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+-- Insert data for 8 bears
+INSERT INTO bears (bear_name, bear_age, bear_sex, bear_color, bear_temperament, bear_alive)
+VALUES('Mr. Chocolate', 5, 'M', 'Brown', 'Friendly', 1),
+    ('Rowdy', 3, 'M', 'Black', 'Wild', 1),
+    ('Tabitha', 2, 'F', 'White', 'Calm', 1),
+    ('Sergeant Brown', 4, 'M', 'Brown', 'Strict', 1),
+    ('Melissa', 6, 'F', 'Honey', 'Playful', 1),
+    ('Grinch', 7, 'M', 'Green', 'Grumpy', 1),
+    ('Wendy', 2, 'F', 'Pink', 'Friendly', 1),
+    ('Unnamed', NULL, 'M', NULL, NULL, 0);
